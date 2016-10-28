28881973db249fe62c5aa678b2ddb5e52afdda07
Marko Tavcar <mare185@gmail.com>
2016-10-28 12:43:06 +0200

    models prvič popravljeno/dodano

--- models/user.py
+++ models/user.py
@@ -4,7 +4,6 @@ from google.appengine.ext import ndb
 class User(ndb.Model):
     email = ndb.StringProperty()
     full_name = ndb.StringProperty()
-    street_name = ndb.StringProperty()  #Naslov tukaj razen ce kasneje dodamo Address kot svoj model?
     address_line_1 = ndb.StringProperty()
     address_line_2 = ndb.StringProperty()
     city = ndb.StringProperty()
@@ -13,8 +12,8 @@ class User(ndb.Model):
     country = ndb.StringProperty()
 
     @classmethod
-    def create(cls, email, full_name, street_name, address_line_1, address_line_2, city, state, zip_number, country):
-        user = cls(email=email, full_name=full_name, street_name=street_name, address_line_1=address_line_1,
+    def create(cls, email, full_name, address_line_1, address_line_2, city, state, zip_number, country):
+        user = cls(email=email, full_name=full_name, address_line_1=address_line_1,
                    address_line_2=address_line_2, city=city, state=state, zip_number=zip_number, country=country)
         user.put()
         return user
